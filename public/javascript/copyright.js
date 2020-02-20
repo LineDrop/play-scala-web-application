90eace221b4517136bbcc64e50f043e14a905d5d
Paul Otrokov <paul.otrokov@gmail.com>
2020-02-20 11:12:22 -0800

    Updated CDN references.

--- public/javascript/copyright.js
+++ public/javascript/copyright.js
@@ -1,4 +1,4 @@
 // update to insert your company name
 $(document).ready(function(){
-    $("#copyright").html('&copy; ' + new Date().getFullYear() + ' LineDrop Code');
+    $("#copyright").html('&copy; ' + new Date().getFullYear() + ' My Company');
 })
